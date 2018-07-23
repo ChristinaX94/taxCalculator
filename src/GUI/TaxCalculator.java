bbd6d26b6ca182769775b3d46dcc6c6dcb052294
Christina <Christina@ADMINRG-DG9MNB6>
2018-07-23 11:04:12 +0300

    First Commit

--- src/GUI/TaxCalculator.java
+++ src/GUI/TaxCalculator.java
@@ -1,3 +1,7 @@
+//Christina Trialoni
+//This is a simple Tax Calculator, a project as part of a University course
+//Uses jfreechart library for barcharts and piecharts (http://www.jfree.org/index.html)
+
 package GUI;
 
 import java.io.IOException;
